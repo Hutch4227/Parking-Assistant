3aa89dad341c99dc7e19ad62a413536888b8ee9a
Austin Lindler <austinlindler@yahoo.com>
2023-06-03 13:30:27 -0500

    Instructions for GitHub

--- Parking Assistant/Program.cs
+++ Parking Assistant/Program.cs
@@ -1,3 +1,8 @@
-﻿Console.WriteLine("Hello, World");
+﻿/*Step 1: Accept invite on GitHub (You must accept invite before you can push code out)
+  Step 2: Sign into GitHub via Visual Studio
+  Step 3: Git --> Clone repository
+  Step 4: Make ammendments to code, commit all, try to sync
+  Step 5: Hope it doesn't take 1.5 hours to figure out again */
+Console.WriteLine("Hello, World");
 Console.WriteLine("Coding is hard");
 Console.WriteLine("Fingers crossed");
